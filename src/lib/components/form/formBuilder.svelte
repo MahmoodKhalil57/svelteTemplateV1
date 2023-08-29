e2ba586a082f82d88b6253ddb5f51b1cfd355590
MahmoodKhalil57 <mahmoodihabk@gmail.com>
2023-08-29 22:01:51 +0100

    init

---
+++ src/lib/components/form/formBuilder.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import FormElement from './formElement.svelte';
+	import type { FormStructure } from '$lib/components/form/inputType';
+
+	export let formStructure: FormStructure;
+</script>
+
+<div class="flex flex-col gap-3">
+	{#each formStructure as row}
+		<div class="flex flex-row gap-2">
+			{#each row as field}
+				<FormElement {field} />
+			{/each}
+		</div>
+	{/each}
+</div>
